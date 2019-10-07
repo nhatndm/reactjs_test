e4f882d36adda0e940e7bb4d1eede09ee155c00e
nhatndm <nhatndm1193@gmail.com>
2019-10-07 09:39:00 +0700

    added state

--- src/components/images/image.js
+++ src/components/images/image.js
@@ -1,13 +1,47 @@
 import React, { Component } from "react";
+import { ReactComponent as HeartSVG } from "../../assest/heart.svg";
 
 export default class Image extends Component {
+  state = {
+    isFavorite: false,
+    imageClass: "not-hover"
+  };
+
+  onMouseEnter = e => {
+    if (!this.state.isFavorite) {
+      this.setState({ imageClass: "hover" });
+    }
+  };
+
+  onMouseLeave = e => {
+    if (!this.state.isFavorite) {
+      this.setState({ imageClass: "not-hover" });
+    }
+  };
+
+  onImageClick = e => {
+    this.setState(prevState => {
+      return {
+        isFavorite: !prevState.isFavorite,
+        imageClass: !prevState.isFavorite ? "liked" : "hover"
+      };
+    });
+  };
+
   render() {
     const {
-      props: { imgSrc }
+      props: { imgSrc },
+      state: { imageClass }
     } = this;
     return (
-      <div className="col-12 col-sm-6 col-md-3 image-item">
+      <div
+        className="col-12 col-sm-6 col-md-3 image-item"
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+        onClick={this.onImageClick}
+      >
         <img className="img-fluid" src={imgSrc} alt="item" />
+        <HeartSVG className={imageClass} />
       </div>
     );
   }
